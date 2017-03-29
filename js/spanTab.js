4a88674fb1104c7d9d687fb73d0162c10e6c9409
Michael Moghaddam <mmoghaddam6@gatech.edu>
2017-03-28 22:41:07 -0400

    added flag backgrounds

--- js/spanTab.js
+++ js/spanTab.js
@@ -7,24 +7,34 @@
 		transltrUtils = window.transltrUtils;
 		wordnikUtils = window.wordnikUtils;
 
-		wordnikUtils.randomWord().then(function (result) {
-			
-			wordnikUtils.definition(result.word).then(function(result) {
-
-				transltrUtils.translate(result[0].word).then(function(translation) {
-
-					alert('The word is ' + result[0].word + '.\nIt is a(n) ' + result[0].partOfSpeech + ' that means ' + result[0].text + '\nIt translates to ' + translation.translationText);
-
-				})
-
-			}, function (errorText) {
-				alert(errorText);
-			})
-
-		}, function(errorText) {
-			alert(errorText);
-		});
+		getRandomWordAndMetadata().then(function(result) {
+			alert('word: ' + result.word + '\ndefinition: ' + result.definitions[0].text + '\nTranslation: ' + result.translation);
+		})
 
 	});
+
+	function getRandomWordAndMetadata() {
+		return new Promise(function(resolve, reject) {
+			wordnikUtils.randomWord().then(function (randomWord) {
+				wordnikUtils.definition(randomWord.word).then(function(definitions) {
+					transltrUtils.translate(randomWord.word).then(function(translation) {
+						let result = {
+							word: randomWord.word,
+							definitions: definitions,
+							translation: translation.translationText
+						};
+
+						resolve(result);
+					}, function(errorText) {
+						reject(errorText);
+					});
+				}, function (errorText) {
+					reject(errorText);
+				});
+			}, function(errorText) {
+				reject(errorText);
+			});
+		});
+	}
 
 })();
